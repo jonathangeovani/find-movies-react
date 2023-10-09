8c72aa04a29cff9efa32236c9477eca8e9d0928c
jonathangeovani <dongeovanimenor@gmail.com>
2023-10-09 10:27:39 -0300

    feat: add routes to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { About, Home } from "./pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 let storedLanguage = localStorage.getItem("LANG");
 
@@ -8,57 +10,21 @@ const App = () => {
   const [language, setLanguage] = storedLanguage
     ? useState(storedLanguage)
     : useState("pt-BR");
-  const [page, setPage] = useState("home");
 
   useEffect(() => {
     localStorage.setItem("LANG", language);
   }, [language]);
 
   return (
-    <div className="app">
-      <nav>
-        {language === "pt-BR" ? (
-          <div className="languages">
-            <span
-              onClick={() => {
-                setLanguage("en-US");
-              }}
-            >
-              EN
-            </span>
-            |
-            <span className="activated" onClick={() => setLanguage("pt-BR")}>
-              BR
-            </span>
-          </div>
-        ) : (
-          <div className="languages">
-            <span
-              className="activated"
-              onClick={() => {
-                setLanguage("en-US");
-              }}
-            >
-              EN
-            </span>
-            |<span onClick={() => setLanguage("pt-BR")}>BR</span>
-          </div>
-        )}
-        {language === "pt-BR" ? (
-          <div className="nav-links">
-            <a onClick={() => setPage("home")}>Página Inicial</a>
-            <a onClick={() => setPage("about")}>Sobre</a>
-          </div>
-        ) : (
-          <div className="nav-links">
-            <a onClick={() => setPage("home")}>Home</a>
-            <a onClick={() => setPage("about")}>About</a>
-          </div>
-        )}
-      </nav>
-      {page === "home" && <Home language={language} />}
-      {page === "about" && <About language={language} />}
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar language={language} setLanguage={setLanguage} />
+        <Routes>
+          <Route path="/" element={<Home language={language} />} />
+          <Route path="/about" element={<About language={language} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+
+interface NavbarProps {
+  language: string;
+  setLanguage: (newLanguage: string) => void;
+}
+
+export default function Navbar(props: NavbarProps) {
+  return (
+    <nav>
+      {props.language === "pt-BR" ? (
+        <div className="languages">
+          <span
+            onClick={() => {
+              props.setLanguage("en-US");
+            }}
+          >
+            EN
+          </span>
+          |
+          <span
+            className="activated"
+            onClick={() => props.setLanguage("pt-BR")}
+          >
+            BR
+          </span>
+        </div>
+      ) : (
+        <div className="languages">
+          <span
+            className="activated"
+            onClick={() => {
+              props.setLanguage("en-US");
+            }}
+          >
+            EN
+          </span>
+          |<span onClick={() => props.setLanguage("pt-BR")}>BR</span>
+        </div>
+      )}
+      {props.language === "pt-BR" ? (
+        <div className="nav-links">
+          <Link to="/">Página Inicial</Link>
+          <Link to="/about">Sobre</Link>
+        </div>
+      ) : (
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </div>
+      )}
+    </nav>
+  );
+}
