d6966846346ff8f5d874e220f77cbf2efb6f2097
jonathangeovani <dongeovanimenor@gmail.com>
2023-10-09 18:18:28 -0300

    feat: create a 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import "./App.css";
+import PageNotFound from "./pages/PageNotFound";
 
 let storedLanguage = localStorage.getItem("LANG");
 
@@ -23,6 +24,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home language={language} />} />
           <Route path="/about" element={<About language={language} />} />
+          <Route path="/*" element={<PageNotFound />} />
         </Routes>
       </main>
       <Footer language={language} />
---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,8 @@
+export default function PageNotFound() {
+  return (
+    <div className="center-content">
+      <h1>Page not found! &nbsp;</h1>
+      <p>The requested page does not exist! &nbsp; &nbsp;</p>
+    </div>
+  );
+}
