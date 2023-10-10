8dfbbd3e1f33c7f5e505b5c53f04e8313f5adf93
jonathangeovani <dongeovanimenor@gmail.com>
2023-10-10 10:59:30 -0300

    feat: add language support to the 404 page

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,8 +1,20 @@
+import { useAppContext } from "../hooks/useAppContext";
+
 export default function PageNotFound() {
+  const { language } = useAppContext();
+
   return (
     <div className="center-content">
-      <h1>Page not found! &nbsp;</h1>
-      <p>The requested page does not exist! &nbsp; &nbsp;</p>
+      <h1>
+        {language == "pt-BR" ? "Página não encontrada" : "Page not found"}!
+        &nbsp;
+      </h1>
+      <p>
+        {language == "pt-BR"
+          ? "A página solicitada não existe"
+          : "The requested page does not exist"}
+        . &nbsp; &nbsp;
+      </p>
     </div>
   );
 }
