31c751b1615f7a6973d5285726a60e92fde7bd0b
jonathangeovani <dongeovanimenor@gmail.com>
2024-02-20 12:39:55 -0300

    rename src/test to src/tests

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: ["./src/test/setup.ts"],
+    setupFiles: ["./src/tests/setup.ts"],
   },
 });
